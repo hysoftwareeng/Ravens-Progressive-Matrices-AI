8edb61d45dd00a4627658dcfda9fa3549ef40e70
hysoftwareeng <harveyyang2002@gmail.com>
2017-09-07 22:47:36 -0400

    Add some boilerplate code
    Add some boilerplate code to parse the problem information

--- Agent.py
+++ Agent.py
@@ -9,8 +9,9 @@
 # These methods will be necessary for the project's main method to run.
 
 # Install Pillow and uncomment this line to access image processing.
-#from PIL import Image
-#import numpy
+from PIL import Image
+import numpy
+from RavensHelper import retrieve_verbal_attributes
 
 class Agent:
     # The default constructor for your Agent. Make sure to execute any
@@ -31,4 +32,18 @@ class Agent:
     # Make sure to return your answer *as an integer* at the end of Solve().
     # Returning your answer as a string may cause your program to crash.
     def Solve(self,problem):
+        #Skip 3x3 problems for the first Project
+        if problem.problemType == '3x3':
+            print
+            return -1
+
+        print ('Beginning to solve problem {} of type {}'.format(problem.name, problem.problemType))
+        problem_figures = {}
+        for name in problem.figures:
+            figure = problem.figures[name]
+            figure_objects = figure.objects
+            retrieve_verbal_attributes(name, figure_objects)
+            file_name = figure.visualFilename
+            image = Image.open(file_name)
+            problem_figures[name] = image
         return -1
---
+++ RavensHelper.py
@@ -0,0 +1,4 @@
+#Retrieve the verbal attributes from a RavensObject
+def retrieve_verbal_attributes(self, object):
+    for name in object:
+        print(name)
